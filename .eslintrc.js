d880f9102afcb3eb55897018fe478b23162df131
denis.anofriev <denis.anofriev@alarislabs.com>
2022-04-04 15:28:45 +0300

    EsLint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 	'rules': {
 		'linebreak-style': [
 			'error',
-			'windows'
+			'unix'
 		],
 		'semi': [
 			'error',
@@ -19,19 +19,19 @@ module.exports = {
 		],
 		'capitalized-comments': 'error',
 		'no-inline-comments': 'error',
-		'multiline-comment-style': ['error','separate-lines'],
-		'max-len': ['error', { 'code' : 120 }],
+		'multiline-comment-style': ['error', 'separate-lines'],
+		'max-len': ['error', { 'code': 120 }],
 		'no-mixed-spaces-and-tabs': 'error',
-		'indent':['error', 'tab'],
-		'object-curly-spacing': ['error', 'always', { 'arraysInObjects': false } ],
+		'indent': ['error', 'tab' , { 'SwitchCase': 1 } ],
+		'object-curly-spacing': ['error', 'always', { 'arraysInObjects': false }],
 		'comma-dangle': ['error', 'never'],
 		'dot-notation': 'error',
 		'new-cap': 'error',
-		'object-curly-newline': ['error',{ 'consistent': true }],
+		'object-curly-newline': ['error', { 'consistent': true }],
 		'no-multi-spaces': 'error',
 		'quotes': ['error', 'single'],
 		'camelcase': 'error',
 		'space-before-blocks': 'error',
-		'no-var': 'error'
+		'no-var': 'error',
 	}
 };
